cf35c545dbd4f6006459de3cfe6f1f3076e0488d
Trada Savan <81802024+savantrada0@users.noreply.github.com>
2023-01-27 10:39:37 +0530

    files added

---
+++ client/build/precache-manifest.9b66a14c3eb586ec78f10992c4357d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a58683c452c8ef75825b75b36bb3d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4510daed4c6873f4fc44",
+    "url": "/static/css/main.5939efef.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4510daed4c6873f4fc44",
+    "url": "/static/js/main.6e1cdb30.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
